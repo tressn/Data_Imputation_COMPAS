9c2bb70123031d1270d433e1378b946a740397fc
Tamara Nelson-Fromm <tamaranf7@gmail.com>
2021-11-17 19:27:52 -0500

    file without missing attributes

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import random
 
 # read data
-data = pd.read_csv("data/compas-scores-two-years.csv", header=0, usecols=['sex', 'age_cat', 'race', 'priors_count', 'c_charge_degree', 'two_year_recid'])
+data = pd.read_csv("compas-scores-two-years.csv", header=0, usecols=['sex', 'age_cat', 'race', 'priors_count', 'c_charge_degree', 'two_year_recid'])
 
 # remove other races
 data = data.drop(data[((data.race != 'African-American') & (data.race != 'Caucasian'))].index)
@@ -16,6 +16,8 @@ print(data['race'].value_counts())
 data = data.drop(data[data['race'] == 1].sample(frac=0.336).index)
 
 print(data['race'].value_counts())
+print(data)
+data.to_csv("COMPAS_processed.csv")
 
 # randomly create missing values
 data.loc[data.sample(frac=0.08).index, 'priors_count'] = ""
@@ -24,4 +26,4 @@ data.loc[data.sample(frac=0.08).index, 'sex'] = ""
 print(data['sex'].value_counts())
 
 print(data)
-data.to_csv("data/COMPAS_processed.csv")
+data.to_csv("COMPAS_processed_missing.csv")
