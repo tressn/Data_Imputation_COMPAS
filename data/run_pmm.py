fc05e0d24aed6fb421b2855563a114b3c1a85bf4
Tamara Nelson-Fromm <tamaranf7@gmail.com>
2021-12-14 14:57:53 -0600

    data that wasn't in the repo opps

--- data/run_pmm.py
+++ data/run_pmm.py
@@ -2,14 +2,7 @@ import numpy as np
 import pandas as pd
 from autoimpute.imputations import SingleImputer, MultipleImputer, MiceImputer
 
-imp = MiceImputer(
-    n=10,
-    strategy={"sex": "pmm", "priors_count": "pmm"},
-    predictors={"sex": "all", "priors_count": "all"},
-    imp_kwgs={"pmm": {"fill_value": "random"}},
-    visit="left-to-right",
-    return_list=True
-)
+imp = MultipleImputer()
 data = pd.read_csv('COMPAS_processed_missing.csv')
 
 data_imputed = imp.fit_transform(data)
